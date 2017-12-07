c4b1fd5c6f6f80c6be0d9e9c5a5efcbf9a883280
Daniel Imberman <danielryan2430@gmail.com>
2017-12-07 09:41:05 -0600

    [AIRFLOW-1517] Kubernetes Operator

---
+++ airflow/contrib/kubernetes/pod_launcher.py
@@ -0,0 +1,105 @@
+
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import time
+from airflow.contrib.kubernetes.pod import Pod
+from airflow.contrib.kubernetes.kubernetes_request_factory.pod_request_factory import (
+    SimplePodRequestFactory)
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.state import State
+from kubernetes import watch
+from kubernetes.client import V1Pod
+from kubernetes.client.rest import ApiException
+
+from .kube_client import get_kube_client
+
+
+class PodStatus(object):
+    PENDING = 'pending'
+    RUNNING = 'running'
+    FAILED = 'failed'
+    SUCCEEDED = 'succeeded'
+
+
+class PodLauncher(LoggingMixin):
+    def __init__(self, kube_client=None):
+        self.kube_req_factory = SimplePodRequestFactory()
+        self._client = kube_client or get_kube_client()
+        self._watch = watch.Watch()
+
+    def run_pod_async(self, pod):
+        req = self.kube_req_factory.create(pod)
+        self.log.debug('Pod Creation Request: \n{}'.format(json.dumps(req, indent=2)))
+        try:
+            resp = self._client.create_namespaced_pod(body=req, namespace=pod.namespace)
+            self.log.debug('Pod Creation Response: {}'.format(resp))
+        except ApiException:
+            self.log.exception('Exception when attempting to create Namespaced Pod.')
+            raise
+        return resp
+
+    def run_pod(self, pod):
+        # type: (Pod) -> State
+        """
+        Launches the pod synchronously and waits for completion.
+        """
+        resp = self.run_pod_async(pod)
+        final_status = self._monitor_pod(pod)
+        return final_status
+
+    def _monitor_pod(self, pod):
+        # type: (Pod) -> State
+
+        while self.pod_is_running(pod):
+            self.log.info("Pod {} has state {}".format(pod.name, State.RUNNING))
+            time.sleep(2)
+        return self.read_pod(pod)
+
+        # for event in self._watch.stream(self.read_pod(pod), pod.namespace):
+        #     status = self._task_status(event)
+        #     if status == State.SUCCESS or status == State.FAILED:
+        #         return status
+
+    def _task_status(self, event):
+        # type: (V1Pod) -> State
+        task = event['object']
+        self.log.info(
+            "Event: {} had an event of type {}".format(task.metadata.name,
+                                                       event['type']))
+        status = self.process_status(task.metadata.name, task.status.phase)
+        return status
+
+    def pod_is_running(self, pod):
+        state = self._task_status(self.read_pod(pod))
+        return state != State.SUCCESS and state != State.FAILED
+
+    def read_pod(self, pod):
+        return self._client.read_namespaced_pod(pod.name, pod.namespace)
+
+    def process_status(self, job_id, status):
+        if status == PodStatus.PENDING:
+            return State.QUEUED
+        elif status == PodStatus.FAILED:
+            self.log.info("Event: {} Failed".format(job_id))
+            return State.FAILED
+        elif status == PodStatus.SUCCEEDED:
+            self.log.info("Event: {} Succeeded".format(job_id))
+            return State.SUCCESS
+        elif status == PodStatus.RUNNING:
+            return State.RUNNING
+        else:
+            self.log.info("Event: Invalid state {} on job {}".format(status, job_id))
+            return State.FAILED
---
+++ airflow/contrib/operators/kubernetes_pod_operator.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from builtins import str
+import dill
+import inspect
+import os
+import pickle
+import subprocess
+import sys
+import types
+
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator, SkipMixin
+from airflow.utils.decorators import apply_defaults
+from airflow.utils.file import TemporaryDirectory
+from airflow.contrib.kubernetes import kube_client, pod_generator, pod_launcher
+from textwrap import dedent
+from airflow.utils.state import State
+
+template_fields = ('templates_dict',)
+template_ext = tuple()
+ui_color = '#ffefeb'
+
+
+class KubernetesPodOperator(BaseOperator):
+    def execute(self, context):
+        raise AirflowException("trying to except")
+        try:
+
+            client = kube_client.get_kube_client(in_cluster=False)
+            gen = pod_generator.PodGenerator()
+
+            pod = gen.make_pod(namespace=self.namespace,
+                               pod_id=self.name,
+                               cmds=self.cmds,
+                               arguments=self.arguments,
+                               labels=self.labels,
+                               )
+
+            launcher = pod_launcher.PodLauncher(client)
+            final_state = launcher.run_pod(pod)
+            if final_state != State.SUCCESS:
+                raise AirflowException("Pod returned a failure")
+
+        except:
+            raise AirflowException("Pod Launching failed")
+
+    @apply_defaults
+    def __init__(self,
+                 namespace,
+                 cmds,
+                 arguments,
+                 labels,
+                 name,
+                 task_id,
+                 *args,
+                 **kwargs):
+        super(KubernetesPodOperator, self).__init__(task_id, *args, **kwargs)
+        self.namespace = namespace,
+        self.cmds = cmds
+        self.arguments = arguments
+        self.labels = labels
+        self.name = name
